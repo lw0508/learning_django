ce21baaea3b416a40f83024494a1d30ef299589f
WMX <1916005702@qq.com>
2020-08-01 22:09:37 +0800

    login函数的提交

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse("第一次使用git推送代码")
+
+def login(request):
+    return redirect('/index')
